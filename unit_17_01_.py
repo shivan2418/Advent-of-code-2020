c1027a8da2414909cd66366dc992e97f581266d7
Emil Elgaard <shivan@shivan.dk>
2020-12-22 00:05:53 -0500

    more attempts on 17

--- unit_17_01_.py
+++ unit_17_01_.py
@@ -2,16 +2,30 @@ input = '''.#.
 ..#
 ###'''
 
+
+neighbour_cache = {}
+
+
+def get_adjacent_coordinates(base):
+
+    try:
+        # try to fetch from cache
+        coordinates = neighbour_cache[base]
+    except KeyError:
+        coordinates = []
+        for mod in [1, -1]:
+            for pos in range(3):
+                new_b = list(base)
+                new_b[pos] += mod
+                coordinates.append(tuple(new_b))
+        # save to cache
+        neighbour_cache[base]=coordinates
+
+    return coordinates
+
 def get_adjacent_active(base,matrix):
 
-
-    coordinates = []
-    for mod in [1, -1]:
-        for pos in range(3):
-            new_b = list(base)
-            new_b[pos] += mod
-            coordinates.append(tuple(new_b))
-
+    coordinates = get_adjacent_coordinates(base)
     num_actives = 0
     for cor in coordinates:
         try:
@@ -34,33 +48,58 @@ def setup(input):
 
 matrix = setup(input)
 
+#enlarge cube
 for round in range(6):
 
+    n_coordinates = []
+    [n_coordinates.extend(get_adjacent_coordinates(pos)) for pos in matrix.keys()]
+    new_cubes = []
+    n_coordinates = list(set(n_coordinates))
+    for cor in n_coordinates:
+        try:
+            value = matrix[cor]
+        except KeyError:
+            new_cubes.append( (cor,'.')    )
+
+    for pos,value in new_cubes:
+        matrix[pos]=value
+
+
+def advance_round(matrix):
     pending_changes = []
 
     for pos in matrix.keys():
 
-
-        num_active = get_adjacent_active(pos,matrix)
+        num_active = get_adjacent_active(pos, matrix)
         try:
-            active = matrix[pos]=='#'
+            active = matrix[pos] == '#'
         except KeyError:
             active = False
 
-        if active and num_active in [2,3]:
-            pass
-        elif active and num_active not in [2,3]:
-            pending_changes.append(  (pos,'.') )
+        if active:
+            if num_active in {2,3}:
+                pass
+            else:
+                pending_changes.append((pos, '.'))
 
-        elif not active and num_active == 3:
-            pending_changes.append((pos, '#'))
+        elif not active:
+            if num_active ==3:
+                pending_changes.append( (pos,'#')  )
+            else:
+                pass
 
-        elif not active and num_active != 3:
-            pass
-        else:
-            raise("Something wrong")
 
-    else:
-        # apply pending changes
-        for pos,new_value in pending_changes:
-            matrix[pos]=new_value
+    # apply pending changes
+    for p, new_value in pending_changes:
+        matrix[p] = new_value
+
+    matrix_values = list(matrix.values())
+    print(f"Num active: {matrix_values.count('#')}")
+
+    return matrix
+
+for round in range(6):
+    matrix = advance_round(matrix)
+
+    matrix_values = list(matrix.values())
+    print(f"Num active: {matrix_values.count('#')}")
